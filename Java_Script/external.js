7c72ac2eba46dfd84acc230a1ea2b95b2f28bf38
Srinivasa naresh <srinivasanaresh72@gmail.com>
2021-08-07 20:01:35 +0530

    added external js

---
+++ Java_Script/external.js
@@ -0,0 +1,7 @@
+alert("Welcome to the Website")
+
+console.log("--Welcome--")
+
+document.write("<h1> Welcome to Fascoon Website </h1>");
+
+document.getElementById("external").innerHTML="This is Java Script External File";
