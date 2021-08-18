bdd45d3fa9d41668da9b59ba705e98b0914d6d19
Naresh <srinivasanaresh72@gmail.com>
2021-08-18 21:48:12 +0530

    added ecma variable

---
+++ Advance_JS/ES6variable3.js
@@ -0,0 +1,12 @@
+var firstname = 'Ganesh';
+firstname = 'Naresh'
+var firstname ='Durga'
+
+let lastname = 'Lanka';
+lastname = 'Prasad'
+//var lastname = 'Kunchala';
+
+const salary = 25000;
+//salary = 15000;
+
+console.log('My name is ' +firstname+ ' ' +lastname+ ' and my salary is ' +salary+'');
