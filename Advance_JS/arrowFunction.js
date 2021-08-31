922b7487a391402e2c2c9139209b2565a94d0e07
Naresh <srinivasanaresh72@gmail.com>
2021-08-31 20:22:49 +0530

    added arrow function

---
+++ Advance_JS/arrowFunction.js
@@ -0,0 +1,23 @@
+function hello1(){
+    console.log("Hello from Hello Function");
+}
+
+hello2 = () => {
+    console.log("Hello from Hello arrow function");
+}
+
+
+function abc1(){
+
+    console.log("From abc function");
+}
+
+abc2 = () => {
+    console.log("From abc arrow function");
+}
+
+
+hello1();
+hello2();
+abc1();
+abc2();
