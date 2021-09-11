0eb9c286878c3e7daa5d885ec7ee8931564c33a7
Naresh <srinivasanaresh72@gmail.com>
2021-09-11 18:16:29 +0530

    added statefull and state less in react

---
+++ React_JS/stateful.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class AppHeader extends Component{
+
+    render(){
+
+        return(
+
+            <h1> You are in the React JS </h1>
+        
+        )
+
+            
+    }
+}
---
+++ React_JS/stateless.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function footer(){
+    return(
+
+        <h4> This is React JS File </h4>
+
+    )
+        
+    
+}
