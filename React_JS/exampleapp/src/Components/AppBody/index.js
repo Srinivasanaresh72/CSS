4f3f14ab7990cacd7f4e4df93a190137c9d6f66c
Naresh <srinivasanaresh72@gmail.com>
2021-09-23 07:04:53 +0530

    added header and footer

---
+++ React_JS/exampleapp/src/Components/AppBody/index.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+export default class AppBody extends Component{
+
+    render(){
+
+        return(
+
+            <p> This is React Js App </p>
+        )
+    }
+
+
+
+
+}
---
+++ React_JS/exampleapp/src/Components/AppFooter/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AppFooter(){
+
+    return(
+
+        <h4> Copyright @2020 </h4>
+    )
+}
