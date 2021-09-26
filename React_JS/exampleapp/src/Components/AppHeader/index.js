c34275848fafde2025f5a9a31074764bb11d7244
Naresh <srinivasanaresh72@gmail.com>
2021-09-26 17:38:17 +0530

    added appheader css

--- React_JS/exampleapp/src/Components/AppHeader/index.js
+++ React_JS/exampleapp/src/Components/AppHeader/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './index.css'
 export default function AppHeader(props){
 
     return(
-        <div>
+        <div className="AppHeader">
 
-             <h1> Welcome to React App </h1>
+             <h1 className="Header"> Welcome to React App </h1>
              <h3> Username : {props.username} </h3>
              <h3> Location : {props.location} </h3>
 
