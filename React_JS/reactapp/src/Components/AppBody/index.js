52f9f2cb7d5cefae1a77edcf9e20790c7f07f593
Naresh <srinivasanaresh72@gmail.com>
2021-09-29 23:04:06 +0530

    imported constructor in Appbody

--- React_JS/reactapp/src/Components/AppBody/index.js
+++ React_JS/reactapp/src/Components/AppBody/index.js
@@ -2,9 +2,20 @@ import React, {Component } from 'react'
 
 export default class AppBody extends Component{
 
+    constructor(props){
+
+        super(props);
+        this.state = {
+
+            'firstname' : 'Lanka',
+            'lastname' : 'Naresh'
+
+        }
+    }
+
     render(){
         return(
-            <p> This is the App Body </p>
+            <p> My Name is {this.state.firstname} {this.state.lastname} </p>
         )
     }
 }
