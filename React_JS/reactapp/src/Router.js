b82ef2bc5f43feabb6f2df5a6ec425b007772fb6
Naresh <srinivasanaresh72@gmail.com>
2021-10-08 08:07:24 +0530

    added router

--- React_JS/reactapp/src/Router.js
+++ React_JS/reactapp/src/Router.js
@@ -1,21 +1,19 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import {Route, Switch } from 'react-router-dom'
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Photos from './Components/Photos';
-import App from './App'
-
 
 function Router() {
 
   return (
     <Switch>
-      <Route path="/" component = {App} />
-      <Route path="/about" component = {About} />
-      <Route path="/contact" component = {Contact} /> 
-      <Route path="/photos" component = {Photos} />
-      
+      <Route path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />  
+      <Route path="/photos" component={Photos} />   
     </Switch>
 
   );
