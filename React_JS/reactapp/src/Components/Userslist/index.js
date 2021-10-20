fbb622db624f971bc7692f05930d112bc483bce1
Naresh <srinivasanaresh72@gmail.com>
2021-10-20 07:39:12 +0530

    added

---
+++ React_JS/reactapp/src/Components/Userslist/index.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+export default class Userslist extends Component{
+
+    render(){
+
+        return(
+
+            <h1> List of Users </h1>
+        )
+    }
+
+
+}
