5c7b06ee916558bc42734f8259af97b41a4aebc4
Naresh <srinivasanaresh72@gmail.com>
2021-10-21 23:09:42 +0530

    added json server

--- React_JS/reactapp/src/Components/Userslist/index.js
+++ React_JS/reactapp/src/Components/Userslist/index.js
@@ -1,11 +1,42 @@
 import React,{Component} from 'react'
+import axios from 'axios'
 export default class Userslist extends Component{
+
+    state = {
+
+        users: []
+    }
+
+    componentDidMount(){
+
+        axios.get('http://localhost:3004/users')
+        .then(res => {
+
+            console.log(res.data);
+            this.setState({
+
+                users: res.data
+
+            })
+            console.log(this.state.users);
+        })
+
+
+    }
 
     render(){
 
         return(
 
+            <div>
+
             <h1> List of Users </h1>
+            {this.state.users.map((user) => (
+
+                <li id={user.id}> {user.name} </li>
+                
+                ))}
+            </div>
         )
     }
 
