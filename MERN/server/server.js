decdd8fc3d28455cdd5e69306c01d23d5b08e5b1
Naresh <srinivasanaresh72@gmail.com>
2021-10-27 22:15:18 +0530

    added server

--- MERN/server/server.js
+++ MERN/server/server.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
+const PORT = 5000;
 
 app.get('/',(req,res)=>{
 
-    res.send('Welcome to Express Server')
+    res.send('<h1>Welcome to Express Server</h1>')
 
 })
 
-const PORT = 5000;
+app.get('/about',(req,res)=>{
+
+    res.send('<h1>Welcome to About Page</h1>')
+
+})
+
+app.get('/users/:id',(req,res)=>{
+
+    const id = req.params.id
+    res.send('<h2>Welcome User: ${id}</h2>')
+})
+
+
 app.listen(PORT,()=>{
 
-    console.log('Server Started at PORT ${PORT}')
+    console.log('Server started at ${PORT}')
     
 })
