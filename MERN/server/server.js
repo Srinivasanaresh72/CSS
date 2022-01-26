f55fcc2f7e86f812cb74f3aa5c875a4587c97fc1
Naresh <srinivasanaresh72@gmail.com>
2022-01-26 17:16:31 +0530

    installed express

---
+++ MERN/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get((req,res)=>{
+
+    res.send('Welcome to Express Server')
+})
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+
+    console.log('Server Started at PORT $(PORT)')
+})
