8ab2a5f93c7e0e8536be878cb3fc878b45de1b70
Naresh <srinivasanaresh72@gmail.com>
2022-01-26 17:59:36 +0530

    added static and dynamic

--- MERN/server/server.js
+++ MERN/server/server.js
@@ -7,8 +7,21 @@ app.get('/',(req,res)=>{
     res.send('Welcome to express server')
 })
 
+app.get('/about',(req,res)=>{
+
+    res.send(`<h1> Welcome to About Page </h1>`)
+
+})
+
+app.get('/users/:id',(req,res)=>{
+
+    const id = req.params.id
+
+    res.send(`<h2> Welcome User ${id} </h2>`)
+})
+
 const PORT = 5000;
 app.listen(PORT,()=>{
 
-    console.log('Server Started at PORT ${PORT}')
+    console.log(`Server Started at PORT ${PORT}`)
 })
